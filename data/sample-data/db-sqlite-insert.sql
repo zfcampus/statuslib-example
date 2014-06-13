9a0060e108078dd09c238577a05c6c69ef705093
agustincl <agustincl@gmail.com>
2014-06-13 07:29:49 +0200

    More files with correct uuid
    For test, some values are needed with the proper id as UUID.

--- data/sample-data/db-sqlite-insert.sql
+++ data/sample-data/db-sqlite-insert.sql
@@ -1,1 +1,4 @@
 INSERT INTO status (id, message, user) VALUES (1, "Hello World", "mwop");
+INSERT INTO status (id, message, user) VALUES ("3b7e0cb2-0565-4645-8512-ed9b86c85bd3", "First post", "mwop");
+INSERT INTO status (id, message, user) VALUES ("2fd4e0fa-e9dd-461d-b0c3-18e29081c155", "Second post", "mwop");
+INSERT INTO status (id, message, user) VALUES ("7a85d61b-5d18-490e-aecd-830452737cc5", "Test validation", "mwop");
