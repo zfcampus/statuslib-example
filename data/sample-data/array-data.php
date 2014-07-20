b9927553fb3805fe502d27951be9bf4a2a71f34e
Bertrand GAUTHIER <bertrand.gauthier@mailoo.org>
2014-07-20 15:30:31 +0200

    Added array sample data
    The file was empty!

---
+++ data/sample-data/array-data.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	[
+		'id' 	  => 1, 
+		'message' => "Hello World", 
+		'user' 	  => "mwop",
+	],
+	[
+		'id' 	  => "3b7e0cb2-0565-4645-8512-ed9b86c85bd3", 
+		'message' => "First post", 
+		'user' 	  => "mwop",
+	],
+	[
+		'id' 	  => "2fd4e0fa-e9dd-461d-b0c3-18e29081c155", 
+		'message' => "Second post", 
+		'user' 	  => "mwop",
+	],
+	[
+		'id' 	  => "7a85d61b-5d18-490e-aecd-830452737cc5", 
+		'message' => "Test validation", 
+		'user' 	  => "mwop",
+	],
+];
