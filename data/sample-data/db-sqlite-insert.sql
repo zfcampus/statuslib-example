dde9a514e329f2e2b5e2eabadc601ab306274874
Matthew Weier O'Phinney <matthew@zend.com>
2014-12-10 16:31:19 -0600

    Use INTEGER for timestamp
    - more portable

--- data/sample-data/db-sqlite-insert.sql
+++ data/sample-data/db-sqlite-insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO status (id, message, user) VALUES (1, "Hello World", "mwop");
-INSERT INTO status (id, message, user) VALUES ("3b7e0cb2-0565-4645-8512-ed9b86c85bd3", "First post", "mwop");
-INSERT INTO status (id, message, user) VALUES ("2fd4e0fa-e9dd-461d-b0c3-18e29081c155", "Second post", "mwop");
-INSERT INTO status (id, message, user) VALUES ("7a85d61b-5d18-490e-aecd-830452737cc5", "Test validation", "mwop");
+INSERT INTO status (id, message, user, timestamp) VALUES (1, "Hello World", "mwop", 1417643129);
+INSERT INTO status (id, message, user, timestamp) VALUES ("3b7e0cb2-0565-4645-8512-ed9b86c85bd3", "First post", "mwop", 1417643200);
+INSERT INTO status (id, message, user, timestamp) VALUES ("2fd4e0fa-e9dd-461d-b0c3-18e29081c155", "Second post", "mwop", 1417643336);
+INSERT INTO status (id, message, user, timestamp) VALUES ("7a85d61b-5d18-490e-aecd-830452737cc5", "Test validation", "mwop", 1417643582);
